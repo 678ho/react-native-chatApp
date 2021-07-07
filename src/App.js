8700b4bb0fdf83679ee6061a406db6e7375a5d35
kwangho kwak <678hoa@gmail.com>
2021-07-07 12:54:35 +0900

    오탈자 수정 및 컴포넌트 수정

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components/native';
-import { theme } from './theme';
+import React, {useState} from 'react';
+import { StatusBar, Image } from 'react-native';
 import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
+import { ThemeProvider } from 'styled-components/native';
+import { theme } from './theme';
 
 const cacheImages = images => {
     return images.map(image => {
-        if (typeof image === 'string') {
-            return Image.prefetch(image);
-        } else {
-            return Asset.fromModule(image).downloadAsync();
-        }
+      if (typeof image === 'string') {
+        return Image.prefetch(image);
+      } else {
+        return Asset.fromModule(image).downloadAsync();
+      }
     });
-};
-
-cosnt cacheFonts = fonts => {
+  };
+  const cacheFonts = fonts => {
     return fonts.map(font => Font.loadAsync(font));
-};
-
-const App = () => {
+  };
+  
+  const App = () => {
     const [isReady, setIsReady] = useState(false);
-
+    
     const _loadAssets = async () => {
         const imageAssets = cacheImages([require('../assets/splash.png')]);
-        const fontAssets = cacheFonts = cacheFonts([]);
+        const fontAssets = cacheFonts([]);
 
         await Promise.all([...imageAssets, ...fontAssets]);
     };
-
     return isReady ? (
-        <ThemeProvider theme={theme} >
-            <StatusBar barStyle={"dark-content"} />
+        <ThemeProvider theme={theme}>
+            <StatusBar barStyle="dark-content" />
         </ThemeProvider>
     ) : (
         <AppLoading
-          startAsync={_loadAssets}
-          onFinish={() => setIsReady(true)}
-          onError={console.warn} />
+            startAsync={_loadAssets}
+            onFinish={() => setIsReady(true)}
+            onError={console.warn} />
     );
-};
+  };
 
-export default App;
+  export default App;
