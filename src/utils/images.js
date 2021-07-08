160aea966b46f76b50baf0f650fb0be14f30f371
kwangho kwak <678hoa@gmail.com>
2021-07-08 12:15:25 +0900

    chat Application job done.

---
+++ src/utils/images.js
@@ -0,0 +1,7 @@
+const prefix =
+  'https://https://firebasestorage.googleapis.com/v0/b/simplechat-4336a.appspot.com/o/';
+
+export const images = {
+  logo: `${prefix}/logo.png?alt=media`,
+  photo: `${prefix}/photo.png?alt=media`,
+};
