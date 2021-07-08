3cc6202ad883d0589a1062e26312960827682f58
kwangho kwak <678hoa@gmail.com>
2021-07-08 11:24:08 +0900

    Reinstall project component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
-import React, {useState} from 'react';
-import { StatusBar, Image } from 'react-native';
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
+import React from 'react';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
-import { theme } from './theme';
-import Navigation from './screens';
+import { theme } from './theme'; 
 
-const cacheImages = images => {
-    return images.map(image => {
-      if (typeof image === 'string') {
-        return Image.prefetch(image);
-      } else {
-        return Asset.fromModule(image).downloadAsync();
-      }
-    });
-  };
-  const cacheFonts = fonts => {
-    return fonts.map(font => Font.loadAsync(font));
-  };
-  
-  const App = () => {
-    const [isReady, setIsReady] = useState(false);
-    
-    const _loadAssets = async () => {
-        const imageAssets = cacheImages([require('../assets/splash.png')]);
-        const fontAssets = cacheFonts([]);
+const App = () => {
+  return (
+    <ThemeProvider theme = {theme}>
+      <StatusBar barStyle="dark-content" />
+    </ThemeProvider>
+  );
+};
 
-        await Promise.all([...imageAssets, ...fontAssets]);
-    };
-    return isReady ? (
-        <ThemeProvider theme={theme}>
-            <StatusBar barStyle="dark-content" />
-            <Navigation />
-        </ThemeProvider>
-    ) : (
-        <AppLoading
-            startAsync={_loadAssets}
-            onFinish={() => setIsReady(true)}
-            onError={console.warn} />
-    );
-  };
-
-  export default App;
+export default App;
